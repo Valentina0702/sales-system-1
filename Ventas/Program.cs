443c4145d6738de595d58d4777c538a21d5ac0e0
Valentina0702 <vdiaz74277@umanizales.edu.co>
2021-03-02 21:52:14 -0500

    second project sales-system-2 with screenshots

--- Ventas/Program.cs
+++ Ventas/Program.cs
@@ -6,6 +6,7 @@ namespace Ventas
     {
         static void Main(string[] args)
         {
+            //Sellers
             SellerWoman seller1 = new SellerWoman();
             seller1.Name = "Ana";
             seller1.Age = 20;
@@ -18,6 +19,7 @@ namespace Ventas
             seller3.Name = "Juana";
             seller3.Age = 40;
 
+            //Products
             Product product1 = new Product();
             product1.Name = "Lápiz";
             product1.Cost = 1000;
@@ -30,6 +32,7 @@ namespace Ventas
             product3.Name = "Cuaderno";
             product3.Cost = 3000;
 
+            //Sale
             Sale s1 = new Sale();
             s1.SellerWoman = seller1.Name;
             s1.Product = product2.Name;
@@ -42,11 +45,13 @@ namespace Ventas
             s2.Coment = 0;
             s2.value = product2.Cost;
 
+
             Sale s3 = new Sale();
             s3.SellerWoman = seller3.Name;
             s3.Product = product3.Name;
             s3.Coment = 1;
             s3.value = product3.Cost;
+
             //¿Cuál es el valor total de las 3 ventas? $4.400
 
             int totalsale(Sale value1, Sale value2, Sale value3)
@@ -57,6 +62,28 @@ namespace Ventas
             }
             
             Console.WriteLine("Sales total amount: " + totalsale(s1, s2, s3));
+
+            SuperMarker databases = new SuperMarker();
+
+            databases.AddSellerWoman(seller1);
+            databases.AddSellerWoman(seller2);
+            databases.AddSellerWoman(seller3);
+
+
+            databases.AddProduct(product1);
+            databases.AddProduct(product2);
+            databases.AddProduct(product3);
+
+            databases.AddSale(s1);
+            databases.AddSale(s2);
+            databases.AddSale(s3);
+            
+
+
+            databases.saleCheapest();  
+            databases.averageValue();
+            databases.productUnit("Lápiz");
+            databases.expensiveProduct();
         }
     }
 }
---
+++ Ventas/SuperMarker.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ventas
+{
+    class SuperMarker
+    {
+        SellerWoman[] SellersWoman = new SellerWoman[20];
+        int sellerCount = 0;
+
+        Product[] Products = new Product[20];
+        int productCount = 0;
+
+        Sale[] Sales = new Sale[20];
+        int saleCount = 0;
+
+        public void AddSellerWoman(SellerWoman newSellerWoman)
+        {
+            SellersWoman[sellerCount] = newSellerWoman;
+            sellerCount++;
+        }
+
+        public void AddProduct(Product newProduct)
+        {
+            Products[productCount] = newProduct;
+            productCount++;
+        }
+
+        public void AddSale(Sale newSale)
+        {
+            Sales[saleCount] = newSale;
+            saleCount++;
+        }
+
+        //More economical product
+
+        public void saleCheapest()
+        {
+            int salevaluetemp = this.Sales[0].value;
+            string salenametemp = this.Sales[0].SellerWoman;
+
+            for (int i = 0; i < this.Sales.Length; i++)
+            {
+                if (this.Sales[i] != null)
+                {
+                    if (this.Sales[i].value < salevaluetemp)
+                    {
+                        salevaluetemp = this.Sales[i].value;
+                        salenametemp = this.Sales[i].SellerWoman;
+                    }
+                }
+            }
+
+            Console.WriteLine("the seller who sold the cheapest product was " + salenametemp);
+
+        }
+
+        // Average value of products
+
+        public void averageValue()
+        {
+            int sumaryvaluetemp = 0;
+            int amountvaluetemp = 0;
+
+            for (int i = 0; i < this.Products.Length; i++)
+            {
+                if (this.Products[i] != null)
+                {
+                    sumaryvaluetemp = sumaryvaluetemp + this.Products[i].Cost;
+                    amountvaluetemp++;
+
+                }
+            }
+
+            Console.WriteLine("the average price is " + (sumaryvaluetemp / amountvaluetemp));
+        }
+
+        // Number of units
+        public void productUnit(string nameProduct)
+        {
+            int countTemp = 0;
+
+            for (int i = 0; i < this.Sales.Length; i++)
+            {
+                if (this.Sales[i] != null)
+                {
+                    if (this.Sales[i].Product == nameProduct)
+                    {
+                        countTemp++;
+
+                    }
+                }
+            }
+
+            Console.WriteLine("the product " + nameProduct + " has been sold " + countTemp);
+        }
+
+        public void expensiveProduct()
+        {
+            int productvaluetemp = this.Products[0].Cost;
+            string productnametemp = this.Products[0].Name;
+
+            for (int i = 0; i < this.Products.Length; i++)
+            {
+                if (this.Products[i] != null)
+                {
+                    if (this.Products[i].Cost > productvaluetemp)
+                    {
+                        productvaluetemp = this.Products[i].Cost;
+                        productnametemp = this.Products[i].Name;
+                    }
+                }
+            }
+
+            Console.WriteLine("the most expensive product was " + productnametemp + " worth " + productvaluetemp);
+
+        }
+    }
+}
